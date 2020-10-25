15a09e99290c2b6449a2531f639f84ed168d52c1
Ishak Ture <ishakture@gmail.com>
2020-10-25 17:54:54 +0000

    Cucumber HW Created

---
+++ src/test/java/runners/CukesRunner.java
@@ -0,0 +1,18 @@
+package runners;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/cucumber.json"},
+
+        dryRun = true,
+        tags = "@wip"
+)
+
+public class CukesRunner {
+
+
+}
